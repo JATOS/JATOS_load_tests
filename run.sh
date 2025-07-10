2019acccdd693b05c1460d794b5386c6e68c3995
Kristian Lange <lange.kristian@gmail.com>
2025-07-10 16:59:38 +0200

    Update `run.sh` to simplify Docker commands and update study code
    Simplified Docker compose commands by removing hardcoded file paths and used the default configuration. Removed unnecessary comment about Gatling logs.

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Gatling logs are in jatos_load_test.log
-
 gatlingCmd="./gatling-charts-highcharts-bundle-3.2.1/bin/gatling.sh"
 
 run () {
@@ -20,28 +18,28 @@ run () {
    description="cpus:$JATOS_CPUS mem:$JATOS_MEM javaXmx:$JAVA_XMX dbPoolSize:$JATOS_DB_CONNECTIONPOOL_SIZE threadPoolSize:$JATOS_THREADPOOL_SIZE $JATOS_HOST $JATOS_STUDY_CODE $SIMULATION $INJECT_TYPE users:$USERS duration:$DURATION successRate:$SUCCESS_RATE"
    echo -n $description
    # Shutdown before start to be sure
-   docker compose -f ~/Downloads/JATOS_with_docker_compose-main/compose.yaml down > /dev/null 2>&1
-   docker compose -f ~/Downloads/JATOS_with_docker_compose-main/compose.yaml up --wait > /dev/null 2>&1
+   docker compose down > /dev/null 2>&1
+   docker compose up --wait > /dev/null 2>&1
    eval "$gatlingCmd -s $SIMULATION -rd '$description' -nr" >> jatos_load_test.log 2>&1 && echo " OK" || echo " KO"
-   docker compose -f ~/Downloads/JATOS_with_docker_compose-main/compose.yaml down > /dev/null 2>&1
+   docker compose down > /dev/null 2>&1
 }
 
 
 # run cpus mem javaXmx dbPoolSize threadPoolSize host studyCode simulation injectType users duration fail
-run 2.0 2G 750M 75 112 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 60 600 100
-run 2.0 2G 750M 75 112 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 70 600 100
-run 2.0 2G 750M 75 112 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 80 600 100
-run 2.0 2G 750M 75 112 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 90 600 100
-run 2.0 2G 750M 75 112 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 100 600 100
+run 2.0 2G 750M 75 112 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 60 600 100
+run 2.0 2G 750M 75 112 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 70 600 100
+run 2.0 2G 750M 75 112 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 80 600 100
+run 2.0 2G 750M 75 112 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 90 600 100
+run 2.0 2G 750M 75 112 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 100 600 100
 
-run 2.0 2G 750M 100 150 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 60 600 100
-run 2.0 2G 750M 100 150 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 70 600 100
-run 2.0 2G 750M 100 150 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 80 600 100
-run 2.0 2G 750M 100 150 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 90 600 100
-run 2.0 2G 750M 100 150 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 100 600 100
+run 2.0 2G 750M 100 150 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 60 600 100
+run 2.0 2G 750M 100 150 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 70 600 100
+run 2.0 2G 750M 100 150 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 80 600 100
+run 2.0 2G 750M 100 150 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 90 600 100
+run 2.0 2G 750M 100 150 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 100 600 100
 
-run 2.0 2G 750M 120 180 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 60 600 100
-run 2.0 2G 750M 120 180 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 70 600 100
-run 2.0 2G 750M 120 180 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 80 600 100
-run 2.0 2G 750M 120 180 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 90 600 100
-run 2.0 2G 750M 120 180 www.example.com B64GEqy93Fe JatosDefaultSimulation atOnceUsers 100 600 100
+run 2.0 2G 750M 120 180 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 60 600 100
+run 2.0 2G 750M 120 180 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 70 600 100
+run 2.0 2G 750M 120 180 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 80 600 100
+run 2.0 2G 750M 120 180 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 90 600 100
+run 2.0 2G 750M 120 180 www.example.com EIAESDcnbB2 JatosDefaultSimulation atOnceUsers 100 600 100
